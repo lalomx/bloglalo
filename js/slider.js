597c4888e3253c2c5554267c75634ecac83bca9c
lalomx <lalo0892@gmail.com>
2021-12-03 17:24:40 -0600

    fix: Add slider

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,11 @@
 var slideWrapperEl = document.querySelector('#posts-slide');
 var slideItms = document.querySelectorAll('#posts-slide .item');
+var navButtons = document.querySelectorAll('button.post__nav__control');
 var slideIdx = 0;
 
-console.log(slideItms);
 var percent = [-25, -50, -75, 0]
 slideItms[slideIdx].classList.toggle('active')
+navButtons[slideIdx].classList.toggle('post__nav__control-active')
 window.setInterval(function () {
   var last = slideIdx
   slideWrapperEl.style.transform = 'translate3d(' + percent[slideIdx] + '%, 0px, 0px)'
@@ -12,6 +13,12 @@ window.setInterval(function () {
   if (slideIdx > percent.length - 1) { 
     slideIdx = 0
   }
+  navButtons[slideIdx].classList.toggle('post__nav__control-active')
+  navButtons[last].classList.toggle('post__nav__control-active')
   slideItms[slideIdx].classList.toggle('active')
   slideItms[last].classList.toggle('active')
 }, 6500)
+
+function navClick(el, idx) {
+  console.table("click", el, idx)
+}
